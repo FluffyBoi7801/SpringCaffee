9f44810d0ffd041f5c5dad6879cc2affd7a1b424
Artem Kuchinskii <artemkka1035@gmail.com>
2021-11-04 23:51:04 +0300

    mainFileCreated

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SpringCoffee</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+	<div class="head">
+		<header>
+			<img class="logo" src="images/logo.svg"></img>
+			<nav>
+				<ul>
+					<li><a href="#benefit">Выгода</a></li>
+					<li><a href="#strategy">Стратегия</a></li>
+					<li><a href="#garanty">Гарантия</a></li>
+					<li><a href="#tariffs">Тарифы</a></li>
+					<li><a href="#faq">FAQ</a></li>
+					<li><a href="#about">О нас</a></li>
+				</ul>
+			</nav>
+		</header>
+	</div>
+</body>
+</html>
