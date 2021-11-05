7e53f76c968f3dd12217067bb9885607e00cfb8c
Artem Kuchinskii <artemkka1035@gmail.com>
2021-11-05 05:31:03 +0300

    thirdBlockFinished

--- index.php
+++ index.php
@@ -24,13 +24,13 @@
 			<p>И ПРОДВИЖЕНИЕ</p>
 			<p>КОФЕЙНЫХ АППАРАТОВ</p>
 			<p class="sub">Мы не просто любим кофе, мы вкладываем часть души!</p>
-			<a href="#">Связаться с нами</a>
+			<a class="contact" href="#">Связаться с нами</a>
 		</div>
 	</div>
 	<div id="benefit">
 		<div class="header">
 			<p>ЧТО ТАКОЕ КОФЕЙНЯ</p>
-			<p class="sub">САМО-ОБСЛУЖИВАНИЯ?</p>
+			<p class="subtext">САМО-ОБСЛУЖИВАНИЯ?</p>
 		</div>
 		<div class="cards">
 			<div class="cardsField">
@@ -68,6 +68,38 @@
 				</div>
 			</div>
 			<img src="images/coffeeMachiche.png" class="coffemchn">
+		</div>
+	</div>
+	<div id="strategy">
+		<div class="header">
+			<p>ПОЧЕМУ РАБОТАТЬ</p>
+			<em>С НАМИ </em><em class="subtext">ВЫГОДНО?</em>
+		</div>
+		<div class="cards">
+			<div class="card">
+				<img src="images/icons/heart.png"/>
+				<p>Работа с нами</p>
+				<p class="sub">Максимально гибкий подход, 
+					вы берете на себя только те вопросы, 
+					которыми хотите заниматься, 
+					все остальное мы берем на себя, 
+					возможен вариант инвестирования</p>
+			</div>
+			<div class="card + black">
+				<img src="images/icons/error.png"/>
+				<p>Собственный<br>бизнес</p>
+				<p class="sub">Оформление, время на регулярный контроль, 
+				документооборот, поиск поставщиков</p>
+			</div>
+			<div class="card + black">
+				<img src="images/icons/error.png"/>
+				<p>Приобретение<br>франшизы</p>
+				<p class="sub">Регулярные взносы, требования франчайзи, 
+				которые мешают<br>развиваться</p>
+			</div>
+		</div>
+		<div>
+			<a class="contact" href="#">Связаться с нами</a>
 		</div>
 	</div>
 </body>
